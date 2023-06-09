06382fee4dbfd62ea19919a34b0b5e217b545157
Dan920-Dev <00138218@uca.edu.sv>
2023-06-09 10:25:25 -0600

    add verification requests view

---
+++ app/src/main/java/com/jder00138218/liftapp/ui/administrator/exerciseManager/DashboardAdministrator.kt
@@ -0,0 +1,212 @@
+package com.jder00138218.liftapp.ui.administrator.exerciseManager
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jder00138218.liftapp.R
+import org.w3c.dom.Text
+
+@Preview(name = "Administrator Dashboard", showBackground = true)
+@Composable
+fun Dashboard() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+
+        Box(modifier = Modifier.fillMaxSize()) {
+
+            Column(
+                Modifier
+                    .align(Alignment.TopCenter)
+                    .fillMaxHeight(0.04f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Solicitudes de verificacion",
+                    color = Color.Black,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp
+                    )
+                )
+            }
+
+
+
+            LazyColumn(
+                Modifier
+                    .align(Alignment.Center)
+                    .fillMaxHeight(0.9f)
+                    .fillMaxWidth()
+            ) {
+                items(20) { index ->
+                    CardExercise()
+                }
+
+            }
+
+            Column(
+                Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxHeight(0.06f)
+                    .fillMaxWidth()
+                    .background(Color.White),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Menu()
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun Menu() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Button(
+            onClick = { /* Acción del primer botón */ },
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(), colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.inbox),
+                contentDescription = "Imbox icon",
+                tint = Color.Red,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+
+        Button(
+            onClick = { /* Acción del segundo botón */ },
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(), colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.profile),
+                contentDescription = "Profile icon",
+                tint = colorResource(id = R.color.gray_text),
+                modifier = Modifier.size(20.dp)
+            )
+        }
+
+        Button(
+            onClick = { /* Acción del tercer botón */ },
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(), colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White
+            )
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.pesa),
+                contentDescription = "Pesa icon",
+                tint = colorResource(id = R.color.gray_text),
+                modifier = Modifier.size(30.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun CardExercise() {
+    Card( // this
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(id = R.color.card)
+        )
+    ) {
+
+        Box(
+            modifier = Modifier
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(modifier = Modifier.fillMaxWidth(1f)) {
+
+                Row() {
+                    Text(text = "Juan Daniel Escobar Rivera", fontWeight = FontWeight.Bold)
+                    // Icon
+                }
+
+
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(1f),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    ItemEx()
+                    ItemEx()
+                }
+
+            }
+        }
+
+
+    }
+}
+
+
+@Composable
+fun ItemEx() {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ), modifier = Modifier
+            .padding(4.dp)
+            .size(width = 160.dp, height = 75.dp)
+    ) {
+        Column(Modifier.padding(8.dp)) {
+            Text(text = "Press de pierna", color = Color.Red)
+            Text(text = "Pecho", color = Color(R.color.gray_text))
+        }
+    }
+
+}

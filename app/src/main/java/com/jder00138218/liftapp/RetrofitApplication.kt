5b99c78cae1ee0c88490c64b15273d916dea0908
Enrique García <54822214+enriquegarcia92@users.noreply.github.com>
2023-06-25 15:00:53 -0600

    Edit: Implementación de repositorio

--- app/src/main/java/com/jder00138218/liftapp/RetrofitApplication.kt
+++ app/src/main/java/com/jder00138218/liftapp/RetrofitApplication.kt
@@ -5,15 +5,23 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.jder00138218.liftapp.network.retrofit.RetrofitInstance
 import com.jder00138218.liftapp.repositories.CredentialsRepository
+import com.jder00138218.liftapp.repositories.ExerciseRepository
 
 class RetrofitApplication: Application() {
     private val prefs: SharedPreferences by lazy {
         getSharedPreferences("Retrofit", Context.MODE_PRIVATE)
     }
 
+    //Servicio para login
     private fun getApiService() = with(RetrofitInstance){
         setToken(getToken())
         getLoginService()
+    }
+
+    //Servicio para ejercicios
+    private fun getExerciseService()= with(RetrofitInstance){
+        setToken(getToken())
+        getExerciseService()
     }
 
 
@@ -21,6 +29,10 @@ class RetrofitApplication: Application() {
 
     val credentialsRepository: CredentialsRepository by lazy {
         CredentialsRepository(getApiService())
+    }
+
+    val exerciseRepository: ExerciseRepository by lazy{
+        ExerciseRepository(getExerciseService())
     }
 
     fun saveAuthToken(token: String){
--- app/src/main/java/com/jder00138218/liftapp/repositories/ExerciseRepository.kt
+++ app/src/main/java/com/jder00138218/liftapp/repositories/ExerciseRepository.kt
@@ -1,4 +1,11 @@
 package com.jder00138218.liftapp.repositories
 
-class ExerciseRepository() {
+import com.jder00138218.liftapp.network.dto.exercise.exercise
+import com.jder00138218.liftapp.network.services.ExerciseService
+
+class ExerciseRepository(private val api:ExerciseService) {
+    suspend fun getSolicitudes(query:String):List<exercise>{
+        val exercises: List<exercise> = api.getSolicitudes(query)
+        return exercises
+    }
 }

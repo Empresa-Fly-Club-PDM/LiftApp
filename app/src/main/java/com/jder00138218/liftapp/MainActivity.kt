b7d97d151edf5dfc518ae17e6198cadef552150f
Enrique García <54822214+enriquegarcia92@users.noreply.github.com>
2023-06-27 14:42:29 -0600

    Working branch

--- app/src/main/java/com/jder00138218/liftapp/MainActivity.kt
+++ app/src/main/java/com/jder00138218/liftapp/MainActivity.kt
@@ -24,10 +24,9 @@ import com.jder00138218.liftapp.ui.register.RegisterScreen
 import com.jder00138218.liftapp.ui.theme.LiftAppTheme
 import com.jder00138218.liftapp.ui.users.admin.exerciseManager.ManageExerciseRequests.DetaileExercise
 import com.jder00138218.liftapp.ui.users.admin.AdminProfile
-import com.jder00138218.liftapp.ui.users.admin.exerciseManager.CreateExercise
+import com.jder00138218.liftapp.ui.users.admin.exerciseManager.CreateExercise.CreateExercise
 import com.jder00138218.liftapp.ui.users.admin.exerciseManager.DescriptionRequest
-import com.jder00138218.liftapp.ui.users.admin.exerciseManager.DetaileExercise
-import com.jder00138218.liftapp.ui.users.admin.exerciseManager.VerifyExercises
+import com.jder00138218.liftapp.ui.users.admin.exerciseManager.VerifiedExerciseView.VerifyExercises
 import com.jder00138218.liftapp.ui.users.user.DashboardUserScreen
 
 class MainActivity : ComponentActivity() {
